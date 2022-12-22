889cae76adc76a6ca2c84da4f694ad1c243e30a7
LSuper <120311070@qq.com>
2022-12-22 23:39:24 +0800

    feat:add leveldb lib

---
+++ LeveldbPimpl.cpp
@@ -0,0 +1,57 @@
+//
+// Created by LMR on 2022/12/22.
+//
+
+#include "LeveldbPimpl.h"
+LeveldbPimpl::LeveldbPimpl():dbName("/data")
+{
+	OpenDB(dbName);
+}
+LeveldbPimpl::~LeveldbPimpl()
+{
+	delete db;
+}
+bool LeveldbPimpl::OpenDB(const std::string& dbName)
+{
+	leveldb::Options options;
+	options.create_if_missing = true;
+	leveldb::Status status = leveldb::DB::Open(options, dbName, &db);
+	if (!status.ok())
+	{
+//		cout << "打开数据失败：dbName:" << dbName << endl;
+		return false;
+	}
+	return true;
+}
+bool LeveldbPimpl::PutData(const std::string &key, const std::string &value)
+{
+	leveldb::Status status = db->Put(leveldb::WriteOptions(), key, value);
+
+	if (status.ok())
+	{
+		return true;
+	}
+	return false;
+}
+std::list<std::string> LeveldbPimpl::GetAllData()
+{
+	std::list<std::string> list;
+	leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+	for (it->SeekToFirst(); it->Valid(); it->Next()) {
+//		spdlog::info("key: {} value: {}", it->key().ToString(), it->value().ToString());
+		list.emplace_back(it->value().ToString());
+	}
+	return list;
+}
+bool LeveldbPimpl::ClearDB()
+{
+	leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+	for (it->SeekToFirst(); it->Valid(); it->Next()) {
+		auto status = db->Delete(leveldb::WriteOptions(), it->key());
+		if (!status.ok())
+		{
+			return false;
+		}
+	}
+	return true;
+}
---
+++ LeveldbPimpl.h
@@ -0,0 +1,31 @@
+//
+// Created by LMR on 2022/12/22.
+//
+
+#ifndef LEVELDBPIMPL_H
+#define LEVELDBPIMPL_H
+
+#include <string>
+#include <list>
+#include "leveldb/db.h"
+
+class LeveldbPimpl
+{
+public:
+	LeveldbPimpl();
+	~LeveldbPimpl();
+
+	bool PutData(const std::string& key, const std::string& value);
+	std::list<std::string> GetAllData();
+	bool ClearDB();
+
+private:
+	bool OpenDB(const std::string &dbName);
+
+private:
+	std::string dbName;
+	leveldb::DB *db;
+};
+
+
+#endif //LEVELDBPIMPL_H
